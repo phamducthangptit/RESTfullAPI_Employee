daa083d55792e625ab792ba1552266d6ac7b860e
phamducthangptit <pthang2506@gmail.com>
2023-12-16 01:24:50 +0700

    update 16/12

--- src/main/java/com/example/RestfullAPI/Controller/EmployeeController.java
+++ src/main/java/com/example/RestfullAPI/Controller/EmployeeController.java
@@ -4,8 +4,6 @@ import com.example.RestfullAPI.DTO.EmployeeDTO;
 import com.example.RestfullAPI.Model.Employee;
 import com.example.RestfullAPI.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
